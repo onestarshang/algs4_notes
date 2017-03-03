afd352fe9185f4631a9f46419eca68908a309b6a
onestarshang <onestar1967@gmail.com>
2017-03-04 00:13:59 +0800

    stack

---
+++ Flips.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.Counter;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class Flips {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+        for (int i = 0; i < n; i++) {
+            if (StdRandom.bernoulli(0.5)) heads.increment();
+            else                          tails.increment();
+        }
+        StdOut.println(heads);
+        StdOut.println(tails);
+        int delta = heads.tally() - tails.tally();
+        StdOut.println("delta: " + Math.abs(delta));
+    }
+}
